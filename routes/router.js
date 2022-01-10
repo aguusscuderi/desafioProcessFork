2d570d4267a3472150914231b49185b8b0481759
aguusscuderi <aguusscuderi9@gmail.com>
2022-01-10 20:17:42 -0300

    done

--- routes/router.js
+++ routes/router.js
@@ -1,13 +1,13 @@
 const { Router } = require('express')
 const router = Router()
 let  { fork }  = require('child_process')
+const child_process = fork('./utils/child_process')
 
 function serverRouter(app){
     app.use('/api', router)
 
     router.get('/random', (req, res)=>{
         let { cant } = req.query
-        const child_process = fork('./utils/child_process')
         if(!cant) child_process.send(100000000)
         else child_process.send(cant)
         child_process.on('message', (response) => {
